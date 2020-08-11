794531502cf6a27747ffb679f41bb3eb21e60fae
DKendle <dina.kendle@gmail.com>
2020-08-11 08:09:39 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,4 @@ class Author < ActiveRecord::Base
     validates :name, presence: true
     validates :email, uniqueness: true
     validates :phone_number, length: {is: 10}
-    validates :title, presence: true 
-    validates :content, length: {minimum: 100}
-    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
