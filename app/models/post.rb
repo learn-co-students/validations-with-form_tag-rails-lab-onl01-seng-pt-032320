6031db562b5a29af1e05cf901adebe9b372844c1
Caroline Longnecker <cvrieous@gmail.com>
2020-09-13 16:27:13 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validate :fiction_or_nonfiction, on: %i[create update]
+
+  def fiction_or_nonfiction
+    return if category == 'Fiction' || category == 'Non-Fiction'
+
+    errors.add(:category, 'must be Fiction or Non-Fiction')
+  end
 end
